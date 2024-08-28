76dc49e7950e2a06d2c2cbe0193be2c6d36cd7ac
Alejandro <soyalebriceno@icloud.com>
2024-08-28 14:49:36 -0600

    commit inicial

---
+++ Tipos de Datos/Tipos-simples.py
@@ -0,0 +1,28 @@
+--UNA LINEA LETRAS
+
+"string"
+'string'
+
+--VARIAS LINEAS
+"""string"""
+
+"""tus datos son:
+nombre: alejandro
+apellido: briceño"""
+
+
+'''string'''
+
+-- NUMEROS
+
+4 INT
+4.4 Float
+
+--BOLEANOS BOOL
+True
+False
+
+
+
+
+
